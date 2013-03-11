8904e4dc5c737253aa2012f05e64636aa23f66ce
Jeremy Smellie <jeremysmellie@gmail.com>
2013-03-11 08:17:10 -0400

    Small changes
    Little changes to the window size andthe checking of the frame size to
    load different asset sizes.

--- Code/Source/Classes/AppDelegate.cpp
+++ Code/Source/Classes/AppDelegate.cpp
@@ -31,11 +31,11 @@ bool AppDelegate::applicationDidFinishLaunching() {
     // this can make sure that the resource's height could fit for the height of design resolution.
 
     // if the frame's height is larger than the height of medium resource size, select large resource.
-	if (frameSize.height > mediumResource.size.height)
+	/*if (frameSize.height > mediumResource.size.height)
 	{ 
 		CCFileUtils::sharedFileUtils()->setResourceDirectory(largeResource.directory);
         pDirector->setContentScaleFactor(MIN(largeResource.size.height/designResolutionSize.height, largeResource.size.width/designResolutionSize.width));
-	}
+	}*/
     // if the frame's height is larger than the height of small resource size, select medium resource.
     /*else if (frameSize.height > smallResource.size.height)
     { 
@@ -43,7 +43,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
         pDirector->setContentScaleFactor(MIN(mediumResource.size.height/designResolutionSize.height, mediumResource.size.width/designResolutionSize.width));
     }*/
     // if the frame's height is smaller than the height of medium resource size, select small resource.
-	else
+	//else
     { 
 		CCFileUtils::sharedFileUtils()->setResourceDirectory(smallResource.directory);
         pDirector->setContentScaleFactor(MIN(smallResource.size.height/designResolutionSize.height, smallResource.size.width/designResolutionSize.width));
